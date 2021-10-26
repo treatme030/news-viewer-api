9e56fef9bc94d41c46a1b31bc86b14e6435a59fd
treatme030 <treatme8192@gmail.com>
2021-10-26 10:25:36 +0900

    promise의 결과에 대한 상태관리하는 usePromise 만들어서 적용

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 import NewsItem from './NewsItem';
+import usePromise from '../lib/usePromise';
 
 const NewsListBlock = styled.div`
     box-sizing: border-box;
@@ -17,32 +18,27 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-    const [articles, setArticles] = useState(null)
-    const [loading, setLoading] = useState(false)
-
-    useEffect(() => {
-        const fetchData = async () => {
-            setLoading(true)
-            try {
-                const query = category === 'all' ? '' : `&category=${category}`
-                const response = await axios.get(
-                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c035a97633fe47b4b41d236f252bd1bd`
-                )
-                setArticles(response.data.articles)
-            } catch(e){
-                console.log(e)
-            }
-            setLoading(false)
-        }
-        fetchData()
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c035a97633fe47b4b41d236f252bd1bd`
+        )
     },[category])
 
+    //대기 중일 때
     if(loading){
         return <NewsListBlock>로딩 중...</NewsListBlock>
     }
-    if(!articles){
+    //아직 response 값이 설정되지 않았을 때 
+    if(!response){
         return null
     }
+    //에러가 발생했을 때
+    if(error){
+        return <NewsListBlock>에러 발생</NewsListBlock>
+    }
+    //response 값이 유효할 때 
+    const { articles } = response.data
     return (
         <NewsListBlock>
             {articles.map(article => (
