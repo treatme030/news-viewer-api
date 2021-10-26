96245bead1de3c0db9d425d029a76c927b4b8587
treatme030 <treatme8192@gmail.com>
2021-10-26 09:52:12 +0900

    라우터의 파라미터를 이용한 카테고리별 API 데이터 가져오기

---
+++ src/components/NewsList.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import NewsItem from './NewsItem';
+
+const NewsListBlock = styled.div`
+    box-sizing: border-box;
+    padding-bottom: 3rem;
+    width: 768px;
+    margin: 0 auto;
+    margin-top: 2rem;
+    @media screen and (max-width: 768px){
+        width: 100%;
+        padding-left: 1rem;
+        padding-right: 1rem;
+    }
+`;
+
+const NewsList = ({ category }) => {
+    const [articles, setArticles] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true)
+            try {
+                const query = category === 'all' ? '' : `&category=${category}`
+                const response = await axios.get(
+                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c035a97633fe47b4b41d236f252bd1bd`
+                )
+                setArticles(response.data.articles)
+            } catch(e){
+                console.log(e)
+            }
+            setLoading(false)
+        }
+        fetchData()
+    },[category])
+
+    if(loading){
+        return <NewsListBlock>로딩 중...</NewsListBlock>
+    }
+    if(!articles){
+        return null
+    }
+    return (
+        <NewsListBlock>
+            {articles.map(article => (
+               <NewsItem key={article.url} article={article}/> 
+            ))}
+            
+        </NewsListBlock>
+    );
+};
+
+export default NewsList;
